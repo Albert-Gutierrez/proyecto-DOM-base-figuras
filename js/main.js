371b36f21d3cb52c293a35905fb4627be0686a5c
albert <alberix252@gmail.com>
2025-09-03 15:44:16 -0500

    se realizo la function para cambio de la figura a auna estrella

--- js/main.js
+++ js/main.js
@@ -2,17 +2,28 @@
 const figura = document.getElementById("figure");
 
 //REALIZAMOS EL CAMBIO DE CUADRADO A CIRCULO
-function cambiarCirculo () {
+function cambiarCirculo() {
     figura.style.borderRadius = "50%";
     figura.style.backgroundColor = "red";
+}
+
+//realizamos el cambio a una estrella
+function cambiarEstrella() {
+    figura.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+}
+
+//colocamos un gif
+function ponerGif() {
+    figura.style.backgroundImage = "url('img/topGun.gif')";
 }
 
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-/*1*/document.getElementById("btnCircle").addEventListener("click", circulo);
 
+document.addEventListener("DOMContentLoaded", function () {
+/*1*/document.getElementById("btnCircle").addEventListener("click", cambiarCirculo);
+/*2*/document.getElementById("btnStar").addEventListener("click", cambiarEstrella);
 
 
 
