ba9f4162641dc43de2e80314b5f5aa2afef743f7
albert <alberix252@gmail.com>
2025-09-03 15:55:29 -0500

    agregue la function poner gif y se agrego la function agregar animacion

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 //aca capturo la figura
 const figura = document.getElementById("figure");
 
-//REALIZAMOS EL CAMBIO DE CUADRADO A CIRCULO
+//1. REALIZAMOS EL CAMBIO DE CUADRADO A CIRCULO
 function cambiarCirculo() {
     figura.style.borderRadius = "50%";
     figura.style.backgroundColor = "red";
 }
 
-//realizamos el cambio a una estrella
+//2. realizamos el cambio a una estrella
 function cambiarEstrella() {
     figura.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
 }
 
-//colocamos un gif
+//3. colocamos un gif
 function ponerGif() {
     figura.style.backgroundImage = "url('img/topGun.gif')";
+    figura.style.backgroundSize = "cover";
 }
+
+//4. Agregar animación → Aplica una animación CSS
+function agregarAnimacion() {
+    figura.classList.toggle("parpadear");
+}
+
+
 
 
 
@@ -24,6 +32,10 @@ function ponerGif() {
 document.addEventListener("DOMContentLoaded", function () {
 /*1*/document.getElementById("btnCircle").addEventListener("click", cambiarCirculo);
 /*2*/document.getElementById("btnStar").addEventListener("click", cambiarEstrella);
+/*3*/document.getElementById("btnGif").addEventListener("click", ponerGif);
+/*4*/document.getElementById("btnAnim").addEventListener("click", agregarAnimacion);
+
+
 
 
 
