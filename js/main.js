6adaff948a696651c1a61580886d52ad519de2b2
albert <alberix252@gmail.com>
2025-09-03 16:43:29 -0500

    agregue function cambiar titulo y function cambiar parrafo con su respectivo addEventListener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //aca capturo la figura
 const figura = document.getElementById("figure");
 
+
+
+//E S T I L O S.........................................................................................................................
 //1. REALIZAMOS EL CAMBIO DE CUADRADO A CIRCULO
 function cambiarCirculo() {
     figura.style.borderRadius = "50%";
@@ -23,22 +26,40 @@ function agregarAnimacion() {
     figura.classList.toggle("parpadear");
 }
 
-//hacemos que 
+//5. hacemos que se mueva hacia arriba
 function moverArriba () {
     figura.style.transform = "translateY(-50px)";
 }
 
+//6. Mover abajo → Desplaza la figura hacia abajo.
 function moverAbajo () {
     figura.style.transform = "translatey(50px)";
 }
 
+//7. Mover izquierda → Desplaza la figura hacia la izquierda.
 function moverIzquierda  () {
     figura.style.transform = "translatex(-50px)";
 }
 
+//8. Mover derecha → Desplaza la figura hacia la derecha.
 function moverDerecha () {
     figura.style.transform = "translatex(50px)";
 }
+
+
+//C O N T E N I D O S..................................................................................................................
+//9. Cambiar título (prompt) → Con prompt, pedir un texto nuevo para el título principal y remplazarlo
+function cambiarTitulo (){
+    const nuevoTitulo = prompt("Escribe un nuevo título:");
+    mainTitle.textContent = nuevoTitulo;
+}
+
+//10. Cambiar párrafo (prompt) → Con prompt, pedir un texto nuevo y reemplazar el contenido del primer párrafo.
+function cambiarParrafo () {
+    const nuevoParrafo = prompt("escribe un parrafo nuevo");
+    mainParagraph.innerText = nuevoParrafo;
+}
+
 
 
 
@@ -53,15 +74,12 @@ document.addEventListener("DOMContentLoaded", function () {
 /*6*/document.getElementById("btnMoveDown").addEventListener("click", moverAbajo);
 /*7*/document.getElementById("btnMoveLeft").addEventListener("click", moverIzquierda);
 /*8*/document.getElementById("btnMoveRight").addEventListener("click", moverDerecha);
-
-
-
-
-
-
+/*9*/document.getElementById("btnChangeTitle").addEventListener("click", cambiarTitulo);
+/*10*/document.getElementById("btnChangeParagraph").addEventListener("click", cambiarParrafo);
 
 
 
 
 
 })
+
